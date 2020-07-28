fc9de6bdfe444c4f40b8deaa037a1eeb69b27fee
Vikas Gupta <vikas.gupti047@gmail.com>
2020-07-28 13:51:44 +0530

    randomcolor

--- src/App.js
+++ src/App.js
@@ -5,22 +5,40 @@ import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 import './style.css'
 import ContactCard from './components/contact';
+import randomcolor from 'randomcolor'
 
 class App extends React.Component {
   constructor() {
     super()
     this.state = {
-      count:0
+      count:0,
+      color: ""
     }
-    this.handleClick = this.handleClick.bind(this)
+    this.increment = this.increment.bind(this)
+    this.decrement = this.decrement.bind(this)
   }
 
-  handleClick() {
+  increment() {
     this.setState(prevState => {
       return{
         count: prevState.count + 1
       }
     }) 
+  }
+  
+  decrement() {
+    this.setState(prevState => {
+      return{
+        count: prevState.count - 1
+      }
+    }) 
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if(prevState.count !== this.state.count){
+      const newColor = randomcolor()
+      this.setState({color: newColor})
+    }
   }
 
   render() {
@@ -32,8 +50,9 @@ class App extends React.Component {
           <ContactCard contact = {{name: "Vikas Gupta", phone: "911"}}/>
           <ContactCard contact = {{name: "Bruce Wayne", phone: "100"}}/>
           <hr/>
-          <p align="center"><h1>{this.state.count}</h1>
-          <button onClick = {this.handleClick}>Change!</button></p>
+          <p align="center"><h1 style={{color: this.state.color}}>{this.state.count}</h1>
+          <button onClick = {this.increment}>Increment</button><br/>
+          <button onClick = {this.decrement}>Decrement</button></p>
       </div>
     );
   }
